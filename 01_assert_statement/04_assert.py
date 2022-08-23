d55d033e07ca5f4d35844bfba161340295fe4b91
metju29 <soltysmate@gmail.com>
2022-08-23 23:17:28 +0200

    Lectures 21-34

---
+++ 01_assert_statement/04_assert.py
@@ -0,0 +1,5 @@
+def area(width, height):
+    """The function returns the area of the rectangle."""
+    return width * height 
+
+assert area(4, 5) == 20
---
+++ 02_unittest_basic/bonus/second.py
@@ -0,0 +1,5 @@
+print(__name__)
+print('[INFO] Skrypt secont.')
+
+import first
+print(first.__name__)
